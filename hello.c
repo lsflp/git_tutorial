11ccda74752db3985029c476bb45a1356545eab6
Luís Felipe <lipeluiis@hotmail.com>
2017-12-14 11:56:45 -0200

    Adicionando interatividade

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main (){
-	printf ("Bye World\n\n");
+	char c;
+	printf("Oi tudo bem?(S ou N):");
+	scanf("%c", &c);
+	if (c == 'S') printf("Ai que bom!\n");
+	else if (c == 'N') printf("PROBLEMA SEU OTARIO KKKKK '-'\n");
 	return 0;
 }
